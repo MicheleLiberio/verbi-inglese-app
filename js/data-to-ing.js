831c77f5bf687220672d7c1b89f0e7565adfae36
MicheleLiberio <50373481+MicheleLiberio@users.noreply.github.com>
2026-05-02 17:48:55 +0200

    Create data-to-ing.js

---
+++ js/data-to-ing.js
@@ -0,0 +1,13 @@
+const toIngVerbs = {
+  to: ["agree","appear","choose","decide","expect","hope","learn","manage","offer","promise","refuse","seem","tend","threaten","want"],
+  ing: ["admit","avoid","bother","deny","dislike","enjoy","finish","get round to","imagine","insist on","keep (on)","mind","miss","suggest"],
+  both: ["begin","continue","start"],
+  change: ["forget","go on","remember","stop","try"]
+};
+
+const toIngLabels = {
+  to: "TO + infinitive",
+  ing: "-ing",
+  both: "TO o -ing",
+  change: "Cambio significato"
+};
