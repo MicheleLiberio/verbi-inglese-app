06e6609865ec194bbe75c975abab6c53c1d3dd47
MicheleLiberio <50373481+MicheleLiberio@users.noreply.github.com>
2026-05-06 21:23:37 +0200

    Create data-reporting.js

---
+++ js/data-reporting.js
@@ -0,0 +1,46 @@
+const reportingVerbs = {
+  that: [
+    "mention",
+    "insist",
+    "explain",
+    "reply",
+    "complain",
+    "claim"
+  ],
+
+  ing: [
+    "recall",
+    "admit",
+    "deny",
+    "recommend",
+    "suggest"
+  ],
+
+  toObject: [
+    "tell",
+    "remind",
+    "persuade",
+    "order",
+    "invite",
+    "forbid",
+    "beg",
+    "ask",
+    "warn"
+  ],
+
+  toInfinitive: [
+    "threaten",
+    "refuse",
+    "promise",
+    "offer",
+    "decide",
+    "agree"
+  ]
+};
+
+const reportingLabels = {
+  that: "(that) + clause",
+  ing: "verb + -ing",
+  toObject: "object + to + infinitive",
+  toInfinitive: "to + infinitive"
+};
