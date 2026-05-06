27190ae578345e35a18158d8df5fc14440c6cb2e
MicheleLiberio <50373481+MicheleLiberio@users.noreply.github.com>
2026-05-06 22:23:20 +0200

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+function showSection(sectionId) {
+  document.querySelectorAll(".section").forEach(section => {
+    section.classList.remove("active");
+  });
+
+  document.getElementById(sectionId).classList.add("active");
+
+  if (sectionId === "toIngPractice") {
+    restartToIng();
+    loadHistory("toIng");
+  }
+
+  if (sectionId === "irregularPractice") {
+    restartIrregular();
+    loadHistory("irregular");
+  }
+
+  if (sectionId === "reportingPractice") {
+    restartReporting();
+    loadHistory("reporting");
+  }
+}
